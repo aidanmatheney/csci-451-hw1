805fbec69c1514120fee077d117fb9a1d8a1cefb
Aidan Matheney <aidanmatheney@gmail.com>
2020-08-30 13:48:38 -0500

    Add error code and message to generateTempFilePath

--- src/util/path.c
+++ src/util/path.c
@@ -4,6 +4,8 @@
 #include "../include/util/error.h"
 
 #include <stdio.h>
+#include <string.h>
+#include <errno.h>
 
 /**
  * Generates a unique file path that does not name a currently existing file.
@@ -15,7 +17,15 @@ char *generateTempFilePath(void) {
     char * const tmpnamResult = tmpnam(tempPath);
     if (tmpnamResult == NULL) {
         free(tempPath);
-        abortWithError("generateTempFilePath: Failed to generate a temporary file path using tmpnam");
+
+        int const tmpnamErrorCode = errno;
+        char const * const tmpnamErrorMessage = strerror(tmpnamErrorCode);
+
+        abortWithErrorFmt(
+            "generateTempFilePath: Failed to generate a temporary file path using tmpnam (error code: %d; error message: \"%s\")",
+            tmpnamErrorCode,
+            tmpnamErrorMessage
+        );
         return NULL;
     }
 
